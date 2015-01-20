0c82c5a2bad6e4d926fc7ac6c5584534bf360e1b
junkal <Loner@Christophers-MacBook-Air.local>
2015-01-20 12:32:32 +0800

    Include simple user instructions

--- ui.R
+++ ui.R
@@ -7,8 +7,8 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
     #Symbol
-    textInput("symbol1", "Enter the symbol of stock 1:", "AAPL"),
-    textInput("symbol2", "Enter the symbol of stock 2:", "GOOG"),
+    textInput("symbol1", "Stock symbol 1:", "AAPL"),
+    textInput("symbol2", "Stock symbol 2:", "GOOG"),
     dateInput("dateFrom", "From:","2014-12-01" ),
     dateInput("dateTo", "To:" ),
     selectInput("marketindex",
@@ -19,12 +19,14 @@ shinyUI(pageWithSidebar(
                              "S&P 500"=4,        
                              "HANG SENG"=5,
                              "NIKKEI 255"=6 ),                  
-                selected=1)
-    #http://stackoverflow.com/questions/23334696/changing-select-input-choices-in-r-shiny
+                selected=1),
+    h4('Instruction'),
+    p('1. Enter the 2 stock symbols to compare'),
+    p('2. Select the time frame'),
+    p('3. Select the market index to compare from the drop-down menu')
   ),
   
   mainPanel(
-    
     plotOutput("symbolPlot"),
     plotOutput("symbolMainIndex")
   )
